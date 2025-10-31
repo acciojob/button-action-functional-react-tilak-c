f17ff5badffadd8bc02fd128ee146f4a137985e2
Tilak <adireddytilak@gmail.com>
2025-10-31 12:13:58 +0530

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React, {Component, useState} from "react";
-import "./../styles/App.css";
+import "../styles/App.css";
 
 function App() {
+  const [show,setShow]=useState(false);
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      {show && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+      <button onClick={()=>setShow(!show)}>Show</button>
     </div>
   );
 }
